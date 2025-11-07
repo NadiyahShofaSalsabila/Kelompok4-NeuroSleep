a84da79f3f9d9009c0707b755d7f19515324937a
Nadiyah Shofa Salsabila <166197389+NadiyahShofaSalsabila@users.noreply.github.com>
2025-11-08 06:29:18 +0700

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./page/Main.js";
+import HomePage from "./page/Home.js";
+import AboutPage from "./page/About.js";
+import LoginPage from "./page/Login.js";
+import RegisterPage from "./page/Register.js";
+import PredictionPage from "./page/Prediction.js";
+import HistoryPage from "./page/History.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<MainPage />}>
+            <Route index element={<HomePage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="predict" element={<PredictionPage />} />
+            <Route path="history" element={<HistoryPage />} />
+          </Route>
+          {/* add other routes as needed */}
+        </Routes>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
