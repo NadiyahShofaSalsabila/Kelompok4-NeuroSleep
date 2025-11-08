88f8c4b902e96f74a536a6d12107d71930b67f24
Nadiyah Shofa Salsabila <166197389+NadiyahShofaSalsabila@users.noreply.github.com>
2025-11-08 12:29:37 +0700

    refactor : mengubah enpoint api

--- frontend/src/page/Login.js
+++ frontend/src/page/Login.js
@@ -28,7 +28,7 @@ const LoginPage = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/login",
+        "/api/login",
         {
           username: formData.username,
           password: formData.password,
--- frontend/src/page/Register.js
+++ frontend/src/page/Register.js
@@ -30,7 +30,7 @@ const RegisterPage = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/register",
+        "/api/register",
         {
           username: formData.username,
           password: formData.password,
