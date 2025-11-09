a27ac72a7dea10ed1f15e2b46e5592fd730972a3
Nadiyah Shofa Salsabila <166197389+NadiyahShofaSalsabila@users.noreply.github.com>
2025-11-09 23:55:15 +0700

    edit app.py untuk testing ci/cd

--- backend/app.py
+++ backend/app.py
@@ -172,5 +172,3 @@ def delete_all_history():
     delete_all_predictions()
     return jsonify({'message': 'All history deleted'})
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=BACKEND_PORT, debug=True)
